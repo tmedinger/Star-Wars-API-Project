109af6578429d4fa28443183dd869a17ff36bbfe
Tony <tmedinger@gmail.com>
2018-05-29 17:32:09 -0400

    Tuesday changes 052918

--- starWarsApi.js
+++ starWarsApi.js
@@ -1,12 +1,38 @@
 const dropdown = document.querySelector (".dropdown");
 const filmSelect = document.querySelector ("select");
-const form = document.querySelector ("form");
-const fieldSet = document.querySelector ("fieldset");
+const subCategoryForm = document.querySelector ("fieldset");
+const baseURL = "https://swapi.co/api";
+const subCategoryValue = document.getElementsByName ("subCategory");
+let url;
 
-form.style.display = "none";
+
+
+subCategoryForm.style.display = "none";
 
 filmSelect.addEventListener("change", filmChoice);
 
-function filmChoice () {
-    form.style.display = "block"
+function filmChoice (e) {
+    subCategoryForm.style.display = "block"
+    e.preventDefault();
+    url = baseURL + "/films/" + filmSelect.value;
+    fetch(url)
+    .then(function(result) {
+        return result.json();
+    })  .then(function(json) {
+        displaySubcategory(json);
+        console.log(json);
+        return json;
+    });
 }
+subCategoryForm.addEventListener("change", displaySubcategory);
+
+function displaySubcategory(json) {
+    for(let v = 0; v < subCategoryValue.length; v ++) {
+        if (subCategoryValue[v].checked){
+            console.log(subCategoryValue[v].value);
+            console.log(json.characters);
+            // console.log(json.subCategoryValue[v].value) 
+            break;
+        } 
+        }
+    }
